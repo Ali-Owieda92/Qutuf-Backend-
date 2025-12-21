95b2f02e24f96f559c3721400b9ef1a4e9906146
Ali <programmerali6@gmail.com>
2025-12-21 12:02:18 +0200

    Change middleware positions

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ cloudinary.config({
 
 const app = express();
 
+app.use(helmet());
 app.use(cors({
     origin: [
         "https://qutufeg.com",
@@ -30,7 +31,8 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Qutuf API is running 🚀');
@@ -51,5 +53,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    connectDB();
 });
