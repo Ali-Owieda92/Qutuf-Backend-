1e806b6f32c704ffe549235a6a683cb780ed8298
Ali Owieda <owiedaali92@gmail.com>
2026-01-09 16:27:43 +0200

    Change origin inside cors to fit the client site

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(helmet());
 app.use(cors({
     origin: [
-        "https://qutufeg.com",
+        "https://www.qutufeg.com",
         "http://localhost:5173",
         "http://localhost:5174"
     ],
